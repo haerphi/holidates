58d6c17116f1c58a71a930569bc367ed0aa1187f
Philippe Haerens <haerphi@gmail.com>
2019-08-26 12:54:44 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+const axios = require('axios');
+const {
+    getCode,
+    getName
+} = require('country-list');
+
+let country = "";
+let year = "";
+//récupération des paramètres
+process.argv.forEach((val, index) => {
+    if (index === 2) {
+        country = getCode(val);
+    } else if (index === 3) {
+        year = val;
+    }
+});
+if (year === "") { //si l'année n'est pas précisé
+    year = new Date().getFullYear();
+}
+if (country != undefined) {
+    //Requête AXIOS
+    let url = `https://date.nager.at/api/v2/publicholidays/${year}/${country}`;
+    console.log(url);
+    axios({
+            method: 'get',
+            url: url
+        })
+        .then(function (response) {
+            //Affichage de toutes les dates + nom
+            for (let i = 0; i < response.data.length; i++) {
+                let text = `${response.data[i].date} - ${response.data[i].localName}`;
+                console.log(text);
+            }
+        }).catch(function (response) {
+            console.log("error");
+            console.log(response.status);
+        });
+}
